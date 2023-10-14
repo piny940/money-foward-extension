99a4843f44e811a21238ba5b47f24e7cc1c4ff53
mikan <83708535+piny940@users.noreply.github.com>
2023-10-14 15:39:07 +0900

    不要なファイルを削除

--- src/content.tsx
+++ src/content.tsx
@@ -1,9 +1,2 @@
 import './styles/global.scss'
 import 'bootstrap'
-import { insertMioLink } from './lib/utils'
-import { onYoutube } from './youtube'
-
-insertMioLink()
-if (window.location.hostname == 'www.youtube.com') {
-  onYoutube()
-}
--- src/resources/constants.ts
+++
@@ -1,2 +0,0 @@
-export const TIME_STAMP_ROOT_EL_ID = 'oyatsu-no-nikka-root'
-export const TIMESTAMP_APP_EL_ID = 'oyatsu-no-nikka-app'
