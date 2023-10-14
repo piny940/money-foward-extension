9e9d38bc25d4e266907cfe36d87e6157fa022a7b
mikan <83708535+piny940@users.noreply.github.com>
2023-10-15 00:12:24 +0900

    saveTransactions, loadTransactionsを作成

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,5 +1,23 @@
 import { _fromStorage, _toStorage } from './_chrome'
+import { TransactionInput } from './types'
 
-export const toStorage = _toStorage
+const toStorage = _toStorage
 
-export const fromStorage = _fromStorage
+const fromStorage = _fromStorage
+
+const getTransactionsKey = (year: number, month: number) => {
+  const TRANSACTIONS_KEY = 'transactions'
+  return `${TRANSACTIONS_KEY}-${year}-${month}`
+}
+export const saveTransactions = async (
+  year: number,
+  month: number,
+  transactions: TransactionInput[]
+) => {
+  await toStorage(getTransactionsKey(year, month), transactions)
+}
+export const loadTransactions = async (year: number, month: number) => {
+  const key = getTransactionsKey(year, month)
+  const data = await fromStorage([key])
+  return data[key] as TransactionInput[]
+}
