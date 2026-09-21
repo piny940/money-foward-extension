820f5f10e1d310e673769d0fbb18e2a33a9bef03
piny940 <83708535+piny940@users.noreply.github.com>
2026-09-21 17:52:24 +0900

    sassをなくしてcssを使う
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- src/content.tsx
+++ src/content.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import React from 'react'
 import { setRootEl } from './lib/site'
-import './styles/global.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import './styles/global.css'
 import 'bootstrap'
 import MoneyApp from './containers/MoneyApp'
 
--- webpack.common.js
+++ webpack.common.js
@@ -29,14 +29,12 @@ module.exports = {
         options: { tsconfig: 'tsconfig.json' },
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.css$/i,
         use: [
           // Creates `style` nodes from JS strings
           'style-loader',
           // Translates CSS into CommonJS
           'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
         ],
       },
     ],
